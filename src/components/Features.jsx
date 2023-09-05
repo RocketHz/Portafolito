91cb05dc6287005bb1c586fe537910ce30e420c2
Ronni Hernandez <ronnihn28@gmail.com>
2023-09-04 22:47:57 -0400

    Optimizaciones generales

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -9,30 +9,26 @@ const Features = () => {
 
     return (
         <>
-            <h2 className="center-title">Proyectos</h2>
-            <p className="center-title">Apartir de aca veras mis proyectos realizados a lo largo de mi vida como programador</p>
             <section id="projects" className="features section">
-                <div className="container">
-                    <div className="features-inner section-inner has-bottom-divider">
-                        <div className="features-wrap ">
-                            {projects.map((project) => (
-                                <div className="feature text-center is-revealing card-project ">
-                                    <div className="feature-inner card2" key={project.id}>
-                                        <div className="feature-icon">
-                                            <img src={project.image} alt="Feature 01" className=""/>
-                                        </div>
-                                        <div>
-                                            <h4 className="feature-title">{project.title}</h4>
-                                            <p className="text-sm">{project.description}</p>
-                                            <div className="btn-group">
-                                                <button onClick={() => redirectToNewTab(project.urlGithub)}>GitHub 🚀</button>
-                                                <button onClick={() => redirectToNewTab(project.url)}>Visit 🚀</button>
-                                            </div>
-                                        </div>
+                <div className="project-container">
+                    <div>
+                        <h2 className="center-title">Proyectos</h2>
+                        <p className="center-title">Apartir de aca veras mis proyectos realizados a lo largo de mi vida como programador</p>
+                    </div>
+                    <div className="features-wrap ">
+                        {projects.map((project) => (
+                            <div className="feature text-center is-revealing card-project ">
+                                <div className="feature-inner card2" key={project.id}>
+                                    <img className="feature-icon" src={project.image} alt="Feature 01" />
+                                    <h4 className="feature-title">{project.title}</h4>
+                                    <p className="text-sm">{project.description}</p>
+                                    <div className="btn-group">
+                                        <button onClick={() => redirectToNewTab(project.urlGithub)}>GitHub 🚀</button>
+                                        <button onClick={() => redirectToNewTab(project.url)}>Visit 🚀</button>
                                     </div>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
