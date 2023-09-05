2ebb7e462c5a32bf3f31a63319035adef7761c8b
Ronni Hernandez <ronnihn28@gmail.com>
2023-09-05 10:33:50 -0400

    Optizaciones del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className="body-wrap">
-        <header className="site-header">
-            <div className="container">
-                <div className="site-header-inner">
-                    <div className="brand header-brand">
-                        <h1 className="m-0">
-                            🚀 Rocket Hz
-                        </h1>
+    return (
+        <div className="body-wrap">
+            <header className="site-header">
+                <div className="header-container">
+                    <div>
+                        <h1>🚀 Rocket Hz</h1>
                     </div>
-                    <div className="brand header-brand">
-                        <ul className="header-link list-reset">
-                            <li>
-                                <a href="#projects">Projects</a>
-                            </li>
-                            <li>
-                                <a href="#about">About us</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className="header-link">
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#about">About me</a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-        </header>
-    </div>
-  )
+            </header>
+        </div>
+    )
 }
 
 export default Header
