efd21f8bab2482285aab3190e949a8e3baeef705
Ronni Hernandez <ronnihn28@gmail.com>
2023-09-08 16:08:53 -0400

    Optimizaciones de css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,16 +33,16 @@ const Hero = () => {
                                     </svg>
                                 </button>
                             </a>
+                            <a href="/FullStackSoftwareDeveloper.pdf" target="_blank" rel="noopener noreferrer">
+                                <button className="social-link2">
+                                    CV
+                                </button>
+                            </a>
                         </div>
                     </div>
 
                     <div className="hero-container">
                         <img className="mark" src="/profile.jpg" alt="Foto de Perfil" />
-                        <a href="/FullStackSoftwareDeveloper.pdf" target="_blank" rel="noopener noreferrer">
-                            <button className="social-link2">
-                                CV
-                            </button>
-                        </a>
                     </div>
                 </div>
             </section>
