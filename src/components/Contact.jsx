7681bf19008250a4ac8ebb6fdfbbe3302e17831a
Ronni Hernandez <ronnihn28@gmail.com>
2023-11-03 16:38:24 -0400

    fixed contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form';
 
 
 const Contact = () => {
+
+  const { register, formState: { errors } } = useForm();
+  
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -11,6 +15,8 @@ const Contact = () => {
     subject: "",
     message: "",
   });
+
+  const [isEmailSend, setIsEmailSend] = useState(false)
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,10 +33,13 @@ const Contact = () => {
     )
     .then((result) => {
         console.log(result.text);
+        setIsEmailSend(true);
     }, (error) => {
         console.log(error.text);
     });
   };
+
+
   return (
     <div id="contact" className="bg-gulf-blue-700 pt-8 py-6 sm:py-8 lg:py-12 rounded">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -48,33 +57,40 @@ const Contact = () => {
         >
           <div>
             <label htmlFor="firstName" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Nombre</label>
-            <input name="firstName" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="firstName" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('firstName', { required: true })} />
+            {errors.firstName && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div>
             <label htmlFor="lastName" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Apellido</label>
-            <input name="lastName" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="lastName" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('lastName', { required: true })} />
+            {errors.lastName && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div>
             <label htmlFor="company" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Empresa</label>
-            <input name="company" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="company" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('company', { required: true })} />
+            {errors.company && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div>
             <label htmlFor="email" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Correo Electronico</label>
-            <input name="email" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="email" type="email" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('email', { required: true })} />
+            {errors.email && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div>
             <label htmlFor="subject" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Asunto</label>
-            <input name="subject" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="subject" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('subject', { required: true })} />
+            {errors.subject && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div>
             <label htmlFor="message" className="mb-2 inline-block text-sm text-gulf-blue-100 sm:text-base">Mensaje</label>
-            <input name="message" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} />
+            <input name="message" type="text" className="w-full rounded border bg-gulf-blue-950 px-3 py-2 text-gulf-blue-400 outline-none ring-gulf-blue-300 transition duration-100 focus:ring" onChange={handleChange} {...register('message', { required: true })} />
+            {errors.message && <span className="text-sm text-red-700">*Requerido</span>}
           </div>
           <div className="flex items-center justify-between sm:col-span-2">
             <button className="inline-block rounded-lg bg-gulf-blue-800 px-8 py-3 text-center text-sm font-semibold text-gulf-blue-200 outline-none ring-gulf-blue-300 transition duration-100 hover:bg-gulf-blue-600 focus-visible:ring active:bg-gulf-blue-700 md:text-base">Enviar</button>
-            <span className="text-sm text-red-700">*Requerido</span>
           </div>
-          <p className="text-xs text-gulf-blue-400">Gracias por contactarme, en un instante te respondere... Atentamente <a href="" className="underline transition duration-100 hover:text-gulf-blue-500 active:text-gulf-blue-600 ">RocketHz</a></p>
+          {isEmailSend && (
+            <p className="text-xs text-gulf-blue-400">Gracias por contactarme, en un instante te respondere... Atentamente <a href="" className="underline transition duration-100 hover:text-gulf-blue-500 active:text-gulf-blue-600 ">RocketHz</a></p>
+          )}
         </form>
       </div>
     </div>
